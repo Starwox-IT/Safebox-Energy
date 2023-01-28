ec9b282cba6b08986d77e630fe2c8862ead06023
vehktaur <victorakhihiero@gmail.com>
2023-01-28 11:04:01 +0100

    Added changes to nav bar, footer and text

--- solar.js
+++ solar.js
@@ -1,13 +1,14 @@
-
 const hamburger = document.querySelector(".hamburger-cunt");
 const hamburgerTop = document.querySelector(".top");
 const hamburgerMid = document.querySelector(".mid");
 const hamburgerBottom = document.querySelector(".bottom");
 const navLinks = document.querySelector(".nav-links");
+const body = document.querySelector("body");
 
 hamburger.addEventListener("click", function () {
   hamburgerTop.classList.toggle("rotate45");
   hamburgerMid.classList.toggle("hidden");
   hamburgerBottom.classList.toggle("rotateNeg45");
   navLinks.classList.toggle("d-none");
+  body.classList.toggle("no_scroll");
 });
